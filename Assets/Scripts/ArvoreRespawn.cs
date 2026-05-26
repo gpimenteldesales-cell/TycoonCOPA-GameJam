5c0bdb35984aad24c7550711a9a0ed9392a1e272
APUCAS <jose_ar_soares@estudante.sesisenai.org.br>
2026-05-26 16:56:22 -0300

    coletar madeira feito
    agora quando uma arvore é cortada o player ganha 3 madeiras

--- Assets/Scripts/ArvoreRespawn.cs
+++ Assets/Scripts/ArvoreRespawn.cs
@@ -1,10 +1,14 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class ArvoreRespawn : MonoBehaviour
 {
+    [Header("Configurações de Tempo")]
     [SerializeField] private float tempoParaCortar = 3f;
     [SerializeField] private float tempoParaRessurgir = 4f;
+    
+    [Header("Configurações de Recompensa")]
+    [SerializeField] private int pontosPorArvore = 3;
 
     private SpriteRenderer spriteRenderer;
     private Collider2D arvoreCollider;
@@ -23,21 +27,32 @@ public class ArvoreRespawn : MonoBehaviour
 
     public void CancelarCorte()
     {
-        if (rotinaCorte != null) StopCoroutine(rotinaCorte);
+        if (rotinaCorte != null)
+        {
+            StopCoroutine(rotinaCorte);
+        }
     }
 
     private IEnumerator CortarAposTempo()
     {
         yield return new WaitForSeconds(tempoParaCortar);
-        spriteRenderer.enabled = false;
-        arvoreCollider.enabled = false;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.AdicionarPontos(pontosPorArvore);
+        }
+
+        if (spriteRenderer != null) spriteRenderer.enabled = false;
+        if (arvoreCollider != null) arvoreCollider.enabled = false;
+
         StartCoroutine(RessurgirRotina());
     }
 
     private IEnumerator RessurgirRotina()
     {
         yield return new WaitForSeconds(tempoParaRessurgir);
-        spriteRenderer.enabled = true;
-        arvoreCollider.enabled = true;
+
+        if (spriteRenderer != null) spriteRenderer.enabled = true;
+        if (arvoreCollider != null) arvoreCollider.enabled = true;
     }
 }
